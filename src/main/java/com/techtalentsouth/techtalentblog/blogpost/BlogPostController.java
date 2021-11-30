07db6aa5ff58d6fbec1f18b36d5f38d497bdf795
Aaron Shank <shank@ctwo.net>
2021-11-30 16:35:55 -0500

    finished styling

--- src/main/java/com/techtalentsouth/techtalentblog/blogpost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/techtalentblog/blogpost/BlogPostController.java
@@ -3,7 +3,6 @@ package com.techtalentsouth.techtalentblog.blogpost;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
